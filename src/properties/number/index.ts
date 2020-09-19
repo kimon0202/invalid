bd5a0c9f748011e46c2154a7014b21f208ace16b
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2020-09-19 18:33:29 -0300

    refactor: reduces the number of files, refactor string properties

--- src/properties/number/index.ts
+++
@@ -1,5 +0,0 @@
-export { minFactory } from './min';
-export { maxFactory } from './max';
-export { lessThanFactory } from './lessThan';
-export { greaterThanFactory } from './greaterThan';
-export { integerFactory } from './integer';
---
+++ src/properties/string.ts
@@ -0,0 +1,56 @@
+/* eslint-disable no-control-regex */
+import { IProperty } from './IProperty';
+import { ValidationError } from '../errors/ValidationError';
+import { defaultMessages } from '../errors/defaultMessages';
+
+const emailRegex = /^((([a-z]|\d|[!#$%&'*+-/=?^_`{|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#$%&'*+\-/=?^_`{|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+const urlRegex = /^((https?|ftp):)?\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!$&'()*+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!$&'()*+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!$&'()*+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!$&'()*+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!$&'()*+,;=]|:|@)|\/|\?)*)?$/i;
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+export const matchesFactory = (regex: RegExp, message?: string): IProperty => ({
+  test: (value: string) => {
+    const isValid = regex.test(value);
+    const error = isValid
+      ? null
+      : new ValidationError(message || defaultMessages.string.matches(regex));
+
+    return [isValid, error];
+  },
+});
+
+export const uuidFactory = (message?: string): IProperty =>
+  matchesFactory(uuidRegex, message || defaultMessages.string.uuid);
+
+export const emailFactory = (message?: string): IProperty =>
+  matchesFactory(emailRegex, message || defaultMessages.string.email);
+
+export const urlFactory = (message?: string): IProperty =>
+  matchesFactory(urlRegex, message || defaultMessages.string.url);
+
+export const maxLengthFactory = (
+  maxValue: number,
+  message?: string,
+): IProperty => ({
+  test: (value: string) => {
+    const isValid = value.length <= maxValue;
+    const error = isValid
+      ? null
+      : new ValidationError(message || defaultMessages.string.max(maxValue));
+
+    return [isValid, error];
+  },
+});
+
+export const minLengthFactory = (
+  minValue: number,
+  message?: string,
+): IProperty => ({
+  test: (value: string) => {
+    const isValid = value.length >= minValue;
+    const error = isValid
+      ? null
+      : new ValidationError(message || defaultMessages.string.min(minValue));
+
+    return [isValid, error];
+  },
+});
